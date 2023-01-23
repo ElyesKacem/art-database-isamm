8bde748e2cfcd80c83319cbf66e78d21d72c42a1
MhamedSanaa <mhamed.sanaa6@gmail.com>
2023-01-23 12:51:46 +0100

    setting up prisma and first steps

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM node:alpine
 
 WORKDIR /usr/app
 COPY package.json .
-COPY prisma ./prisma/
-COPY .env ./
 RUN npm install -g nodemon
 RUN npm install
 COPY . .
--- server.js
+++ server.js
@@ -1,20 +1,53 @@
-var express = require('express'),
-    body_parser = require('body-parser'),
-    pg = require('pg'),
-    app = express();
+const express = require('express');
+const body_parser = require('body-parser');
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+const app = express();
+const port = 3000
 
 app.use(body_parser.json())
 app.use(body_parser.urlencoded({ extended: false }))
 
-const client = new pg.Client({
-    user: 'postgres',
-    host: 'postgre_db',
-    database: 'postgres',
-    password: 'postgres',
-    port: 5432
+
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      posts: {
+        create: { title: 'Hello World' },
+      },
+      profile: {
+        create: { bio: 'I like turtles' },
+      },
+    },
   })
-console.log(client.connect());
-client.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    client.end()
-  });
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+  console.dir(allUsers, { depth: null })
+}
+app.get('/user',(req, res) => {
+  main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+  res.send(main)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
