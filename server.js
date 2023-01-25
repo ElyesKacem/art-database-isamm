39fd9ec945c625a992931184f86bbe7ec7631e45
MhamedSanaa <mhamed.sanaa6@gmail.com>
2023-01-25 18:30:59 +0100

    adding cors for api global consumtion

--- server.js
+++ server.js
@@ -1,30 +1,19 @@
 const express = require('express');
 const body_parser = require('body-parser');
 const app = express();
+const cors = require('cors')
 const port = 3000;
 //configuration middleware
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: false }));
-//const multer = require('multer');
-/*const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
-const upload = multer({ storage: storage });
 
 
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  try {
-    res.send({ url: `/uploads/${req.file.originalname}` });
-  } catch (error) {
-    res.send(error);
-  }
-})*/
+var corsOptions = {
+  origin: '*'
+}
+
+app.use(cors(corsOptions))
 //api middleware list
 app.use('/api', require('./src/routes/route'));
 
